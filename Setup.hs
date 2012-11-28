8f888e1acda22a26a71f95bf14960b6c4878e427
Jeroen Bransen <J.Bransen@uu.nl>
2012-11-28 12:23:01 +0100

    Initial commit

---
+++ Setup.hs
@@ -0,0 +1,6 @@
+import Distribution.Simple (defaultMainWithHooks)
+import Distribution.Simple.UUAGC (uuagcLibUserHook)
+import UU.UUAGC (uuagc)
+
+main :: IO ()
+main = defaultMainWithHooks (uuagcLibUserHook uuagc)
